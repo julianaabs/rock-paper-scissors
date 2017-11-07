c5c337b4efeb13aacc9015fecd73c06f5146dfdb
julianaabs <jubsbarbosa0@gmail.com>
2017-11-06 23:38:06 -0300

    first page

---
+++ rock-paper-scissors.js
@@ -0,0 +1,16 @@
+function clickRock(){
+	alert("You chose Rock!");
+}
+
+function clickPaper(){
+	alert("You chose Paper!");
+}
+
+function clickScissors(){
+	alert("You chose Scissors!");
+}
+
+function enterPlayer(){
+	var name = prompt("Type your name: ");
+	alert(name);
+}
